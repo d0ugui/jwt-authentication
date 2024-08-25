a83903a7482a0dcfddf1585545ebc608e32a150f
d0ugui <douglaspo_97@outlook.com>
2024-08-25 19:48:17 -0300

    feat: create sign-up use-case, prisma client singleton instance and account-already-exists errors type

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -5,6 +5,10 @@ import tseslint from "typescript-eslint";
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
+    linterOptions: {
+      indent: ["error", 2],
+      quotes: ["error", "single"]
+    }
   },
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
