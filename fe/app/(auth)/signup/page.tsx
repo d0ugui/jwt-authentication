94f1d421925b96f078c8f9a7058a065fdf805342
d0ugui <douglaspo_97@outlook.com>
2024-09-04 20:31:10 -0300

    feat: create sign-in and sign-up forms

--- fe/app/(auth)/signup/page.tsx
+++ fe/app/(auth)/signup/page.tsx
@@ -1,29 +1,29 @@
-import { UserAuthForm } from "@/components/UserAuthForm";
+import { UserSignUpForm } from "@/components/UserSignInForm";
 import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Login",
-  description: "Login to your account",
+  title: "Register",
+  description: "Register your account",
 };
 
-export default function Home() {
+export default function SignUp() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center mx-auto">
       <div className="flex w-full flex-col justify-center space-y-6 sm:[w-350px]">
         <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-light">Login</h1>
-          <p>Enter your email to sign in to your account</p>
+          <h1 className="text-2xl font-semibold tracking-light">Sign up</h1>
+          <p>Create your account right now</p>
         </div>
 
-        <UserAuthForm />
+        <UserSignUpForm />
 
         <p className="px-8 text-center text-sm text-muted-foreground">
           <Link
-            href="/signup"
+            href="/"
             className="hover:text-brand underline underline-offset-4"
           >
-            Don&apos;t have an account? Sign Up
+            I already have an account
           </Link>
         </p>
       </div>
---
+++ fe/components/UserSignInForm.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+export function UserSignUpForm() {
+  return (
+    <div className="w-full max-w-[350px] mx-auto">
+      <div className="flex flex-col gap-4">
+        <Label className="sr-only" htmlFor="email">
+          Email
+        </Label>
+        <Input
+          id="email"
+          placeholder="name@example.com"
+          type="email"
+          autoCapitalize="none"
+          autoComplete="email"
+          autoCorrect="off"
+        />
+
+        <Label className="sr-only" htmlFor="password">
+          Password
+        </Label>
+        <Input
+          id="password"
+          placeholder="********"
+          type="password"
+          autoCapitalize="none"
+          autoComplete="email"
+          autoCorrect="off"
+        />
+
+        <Button variant="secondary">Sign up</Button>
+      </div>
+    </div>
+  );
+}
