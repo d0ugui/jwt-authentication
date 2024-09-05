f8d3b661d4c05bcefa143d4126923697551c79c7
d0ugui <douglaspo_97@outlook.com>
2024-09-04 21:22:43 -0300

    refactor: remove unused code and add button to turn back

--- fe/app/(auth)/signup/page.tsx
+++ fe/app/(auth)/signup/page.tsx
@@ -1,6 +1,9 @@
+import { buttonVariants } from "@/components/ui/button";
 import { UserSignUpForm } from "@/components/UserSignInForm";
+import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Register",
@@ -10,7 +13,20 @@ export const metadata: Metadata = {
 export default function SignUp() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center mx-auto">
-      <div className="flex w-full flex-col justify-center space-y-6 sm:[w-350px]">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:left-8 md:top-8"
+        )}
+      >
+        <>
+          <ChevronLeft size={16} className="mr-2" />
+          Back
+        </>
+      </Link>
+
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-light">Sign up</h1>
           <p>Create your account right now</p>
--- fe/components/UserAuthForm.tsx
+++ fe/components/UserAuthForm.tsx
@@ -4,34 +4,32 @@ import { Label } from "./ui/label";
 
 export function UserAuthForm() {
   return (
-    <div className="w-full max-w-[350px] mx-auto">
-      <div className="flex flex-col gap-4">
-        <Label className="sr-only" htmlFor="email">
-          Email
-        </Label>
-        <Input
-          id="email"
-          placeholder="name@example.com"
-          type="email"
-          autoCapitalize="none"
-          autoComplete="email"
-          autoCorrect="off"
-        />
+    <div className="flex flex-col gap-4">
+      <Label className="sr-only" htmlFor="email">
+        Email
+      </Label>
+      <Input
+        id="email"
+        placeholder="name@example.com"
+        type="email"
+        autoCapitalize="none"
+        autoComplete="email"
+        autoCorrect="off"
+      />
 
-        <Label className="sr-only" htmlFor="password">
-          Password
-        </Label>
-        <Input
-          id="password"
-          placeholder="********"
-          type="password"
-          autoCapitalize="none"
-          autoComplete="email"
-          autoCorrect="off"
-        />
+      <Label className="sr-only" htmlFor="password">
+        Password
+      </Label>
+      <Input
+        id="password"
+        placeholder="********"
+        type="password"
+        autoCapitalize="none"
+        autoComplete="email"
+        autoCorrect="off"
+      />
 
-        <Button variant="secondary">Sign in</Button>
-      </div>
+      <Button variant="secondary">Sign in</Button>
     </div>
   );
 }
