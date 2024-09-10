8a654a34fed0ff7e669e18abdbcdddf425adf818
d0ugui <douglaspo_97@outlook.com>
2024-09-10 20:16:01 -0300

    chore: migrate from next to react-ts vite

--- client/app/actions/get-session.ts
+++
@@ -1,9 +0,0 @@
-import { verify } from "jsonwebtoken";
-import { cookies } from "next/headers";
-
-export async function getSession() {
-  const session = cookies().get("session")?.value;
-  if (!session) return null;
-
-  return verify(session, process.env.JWT_SECRET);
-}
