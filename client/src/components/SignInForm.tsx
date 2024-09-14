02f9ce498674e75ace7efcc640a29b5c373bf4f2
d0ugui <douglaspo_97@outlook.com>
2024-09-14 17:37:05 -0300

    feat: install react-hook-form and integrate forms

--- client/src/components/SignInForm.tsx
+++ client/src/components/SignInForm.tsx
@@ -1,10 +1,28 @@
+import { useForm } from "react-hook-form";
 import Button from "./ui/Button";
 import { Input } from "./ui/Input";
 import { Label } from "./ui/Label";
 
+interface IFormData {
+  email: string;
+  password: string;
+}
+
 export function SignInForm() {
+  const form = useForm<IFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = form.handleSubmit(async ({ email, password }) => {
+    console.log("email:", email);
+    console.log("password:", password);
+  });
+
   return (
-    <form className="mt-8 flex flex-col gap-2">
+    <form className="mt-8 flex flex-col gap-2" onSubmit={handleSubmit}>
       <div className="space-y-1">
         <Label htmlFor="email">Email</Label>
         <Input id="email" placeholder="name@example.com" type="email" />
--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -1,10 +1,31 @@
 import { Input } from "./ui/Input";
 import Button from "./ui/Button";
 import { Label } from "./ui/Label";
+import { useForm } from "react-hook-form";
+
+interface IFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 export function SignUpForm() {
+  const form = useForm<IFormData>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = form.handleSubmit(async ({ name, email, password }) => {
+    console.log("name:", name);
+    console.log("email:", email);
+    console.log("password:", password);
+  });
+
   return (
-    <form className="mt-8 flex flex-col gap-2">
+    <form className="mt-8 flex flex-col gap-2" onSubmit={handleSubmit}>
       <div className="space-y-1">
         <Label htmlFor="name">Name</Label>
         <Input id="name" />
