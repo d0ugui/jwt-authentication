a50cd9822a9b6b510a1ee9b27f33551f1ca1c5e5
d0ugui <douglaspo_97@outlook.com>
2024-09-17 20:10:16 -0300

    feat: handle with refreshToken request error

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,6 +1,13 @@
 import { storageKeys } from "@/config/storageKeys";
 import { AuthService } from "@/services/AuthService";
-import { createContext, useCallback, useState } from "react";
+import { httpClient } from "@/services/httpClient";
+import {
+  createContext,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react";
 
 interface IAuthContextValue {
   signedIn: boolean;
@@ -14,6 +21,54 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [signedIn, setSignedIn] = useState(() => {
     return !!localStorage.getItem(storageKeys.accessToken);
   });
+
+  useLayoutEffect(() => {
+    const interceptorId = httpClient.interceptors.request.use((config) => {
+      const accessToken = localStorage.getItem(storageKeys.accessToken);
+
+      if (accessToken) {
+        config.headers.set("Authorization", `Bearer ${accessToken}`);
+      }
+
+      return config;
+    });
+
+    return () => {
+      httpClient.interceptors.request.eject(interceptorId);
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    const interceptorId = httpClient.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+        const refreshToken = localStorage.getItem(storageKeys.refreshToken);
+
+        if (originalRequest.url === "/refresh") {
+          setSignedIn(false);
+          localStorage.clear();
+          return Promise.reject(error);
+        }
+
+        if (error.response?.status !== 401 || !refreshToken) {
+          return Promise.reject(error);
+        }
+
+        const { accessToken, refreshToken: newRefreshToken } =
+          await AuthService.refreshToken(refreshToken);
+
+        localStorage.setItem(storageKeys.accessToken, accessToken);
+        localStorage.setItem(storageKeys.refreshToken, newRefreshToken);
+
+        return httpClient(originalRequest);
+      }
+    );
+
+    return () => {
+      httpClient.interceptors.response.eject(interceptorId);
+    };
+  }, []);
 
   const signIn = useCallback(async (email: string, password: string) => {
     const { accessToken, refreshToken } = await AuthService.signIn({
